e4b0b0dc527d2b434aa40fb14d857f0abb848a0b
JYB <JYB@JYB-PC>
2018-06-13 21:34:23 +0800

    修改后的

--- src/cn/Demo.java
+++ src/cn/Demo.java
@@ -1,10 +1,15 @@
 package cn;
 
+import java.util.Iterator;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word!");
+		//System.out.println("Hello Word!");
+		for (int i = 0; i < 7; i++) {
+			System.out.println("¾«Ó¢°à×î°ô");
+		}
 	}
 
 }
